ee9d0f280f060d5ba7909ffc8ec403b1a55c3372
Jeremias <jerefigueiredo@gmail.com>
2022-04-27 17:59:35 -0300

    configured bundle.js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const firebaseConfig = {
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+console.log("hi");
 /*
 if (location.hostname === "localhost") {
     config = {
