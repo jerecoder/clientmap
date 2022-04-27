26ec44b1baa327e87405a10193d77aee5a5eddfd
Jeremias <jerefigueiredo@gmail.com>
2022-04-27 17:37:43 -0300

    added webpack

--- public/index.mjs
+++ public/index.mjs
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 // https://firebase.google.com/docs/web/setup#available-libraries
-/*
+
 const firebaseConfig = {
     apiKey: "AIzaSyBRrI-nmNq0V4JooyOsQSlS0CeGCVcJZxQ",
     authDomain: "clientmap-b1f1b.firebaseapp.com",
@@ -11,22 +11,23 @@ const firebaseConfig = {
     appId: "1:608817167872:web:6b4498f816b50f2f8562e5",
     measurementId: "G-F9VFEZ3FYF"
 };
-
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+/*
 if (location.hostname === "localhost") {
     config = {
         databaseURL: "http://localhost:4000/firestore"
     };
 }
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 function initMap() {
     let map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: 0, lng: 0 },
         zoom: 3
-    });
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBf8QJr96TN4RwSEsnEghuHlsVUQHpzhic&callback=initMap&v=weekly " defer></script>
-//
-}*/
+    }); <
+    script src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyBf8QJr96TN4RwSEsnEghuHlsVUQHpzhic&callback=initMap&v=weekly "
+    defer > < /script>
+        //
+} */
